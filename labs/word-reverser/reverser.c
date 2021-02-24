835509dc87ff13a18d3a282c3d30df93c97f0668
Ulises Bojorquez <ulibojorquez@gmail.com>
2021-02-24 17:08:26 +0000

    Solve word-reverser
    Signed-off-by: Ulises Bojorquez <ulibojorquez@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#define IN   1   /* inside a word */
+#define OUT  0   /* outside a word */
 
-int main(){
-    // Place your magic here
+int main()
+
+{
+    int i, state;
+    char c, word[100], reverse[100];
+    state = OUT;
+
+    i = 0;
+    int len=0;
+    while ((c = getchar()) != EOF) {    
+        if (c == '\n' || c == '\t') {
+            state = OUT;
+            
+            //printf("%s \n", word);
+            
+            //REVERSE
+            len=i-1;
+            for(int k=0;k<i;k++){
+                reverse[k]=word[len];
+                len--;
+            }
+            printf("%s \n", reverse);
+            
+            memset(&word[0], 0, sizeof(word));
+            memset(&reverse[0], 0, sizeof(reverse));
+
+            i=0;
+            
+        }
+        else if (state == OUT) {
+            state = IN;
+            word[i]=c;
+            i++;
+            
+        }else{
+            word[i]=c;
+            i++;
+        }
+    }
+
     return 0;
 }
