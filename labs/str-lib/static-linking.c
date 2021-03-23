3d68872763906b4ee05c9d5f68197670f5e098ec
Ulises Bojorquez <ulibojorquez@gmail.com>
2021-03-23 06:10:08 +0000

    Solve str-lib
    Signed-off-by: Ulises Bojorquez <ulibojorquez@gmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+
+    if(argc<4){
+        printf("Check your parameters\n");
+        return 0;
+    }else if(strcmp(argv[1],"-add")==0){
+        char *len=argv[2];
+        char* strOrigin=argv[2];
+        char* strAddition=argv[3];
+        printf("Initial Lenght      :%i\n", mystrlen(len));
+        char* strComplete=mystradd(strOrigin,strAddition);
+        printf("New String          :%s\n",strComplete);
+        printf("New length          :%i\n",mystrlen(strComplete));
+    }else if(strcmp(argv[1],"-find")==0){
+        char* strOrigin=argv[2];
+        char* strSubstring=argv[3];
+        if(mystrfind(strOrigin,strSubstring)<0){
+            printf("This substring does not match in the origin string\n");
+            return 0;
+        }
+        printf("['%s'] string was found at [%i] position\n", strSubstring, mystrfind(strOrigin,strSubstring));
+    }
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,61 @@
-
-
 int mystrlen(char *str){
-    return 0;
+
+    int len;
+    for(len=0;str[len]!='\0';len++);
+
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+
+    char *str1=origin;
+    char *str2=addition;
+
+    int i=mystrlen(str1);
+    for(int j=0;str2[j]!='\0';j++)  
+    {  
+        str1[i]=str2[j];  
+        i++;  
+    }  
+    str1[i]='\0';  
+    return str1;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    
+    char *src=origin;
+    char *sub=substr;
+    
+    int i=0;
+    int j=0;
+    int index;
+ 
+    while (src[i] != '\0') {
+        //Start searching the until the src[i] is equal to sub[0]
+        while (src[i] != sub[0] && src[i] != '\0'){
+            i++;
+        }
+        //If it does not find anything
+        if (src[i] == '\0'){
+            return -1;
+        }
+ 
+        index = i;
+        //If match
+        while (src[i] == sub[j] && src[i] != '\0' && sub[j] != '\0') {
+            i++;
+            j++;
+        }
+        //If we are at the end of the subtring
+        if (sub[j] == '\0'){
+            return index;
+        }
+        //If we read all the source and does not find the substring
+        if (src[i] == '\0'){
+            return -1;
+        }
+
+        i = index + 1;
+        j = 0;
+   }
 }
